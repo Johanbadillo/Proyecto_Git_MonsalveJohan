a364e63ea41276b9e59363a8d1cbe952f863038f
johan <johanbadillomj@gmail.com>
2025-06-03 13:10:37 -0500

    feat:sparkles: start the addEmpanadas

--- Mian.py
+++ Mian.py
@@ -1,17 +1,33 @@
 # #######################################
 # ######### Proyecto git prueba #########
 # #####################################
+from Funciones.funciones import *
+salida=True
 
-print("Bienvenido a nuestro programa de empanadas Doña Pepa\
-    \n\
-    \n=====================================\
-    \n           Menu Principal             \
-    \n======================================\
-    \nGestion de Datos:\
-    \n1. Agregar dato nuevo\
-    \n2. Editar dato\
-    \n3. Eliminar dato\
-    \n4. Salir")
+while(salida):
+    print("Bienvenido a nuestro programa de empanadas Doña Pepa\
+        \n\
+        \n=====================================\
+        \n           Menu Principal             \
+        \n======================================\
+        \nGestion de Datos:\
+        \n1. Agregar dato nuevo\
+        \n2. Editar dato\
+        \n3. Eliminar dato\
+        \n4. Salir")
+    Opcion=int(input("Ingrese una opcion numerica\n"))
+    if(Opcion==1):
+        Nombre=str(input("Ingrese el nombre de la empanada: "))
+        precio=int(input("Ingrese el precio de la empanada sin puntos ni comas: "))
+        Caningredientes=int(input("Ingrese la cantidad de ingredientes que tiene la empanada: "))
+        diccionarioNuevo={
+            "nombre":Nombre,
+            "precio":precio,
+            "ingredientes":[]
+        }
+        for i in range (Caningredientes):
+            nomIngrediente=str(input("Ingrese el nombre del ingrediente: "))
+            disponibilidad=int(input("¿El ingrediente esta disponible?\n1. Si\n2. No\nIngrese una opcion numerica "))
 
 
 
@@ -25,4 +41,6 @@ print("Bienvenido a nuestro programa de empanadas Doña Pepa\
 
 
 
-# que pasa si hago esto??
+
+
+# Pruebas 
