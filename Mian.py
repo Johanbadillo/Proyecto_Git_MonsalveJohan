270d1ea713957b268cc2ab519bf002a117bc0f97
johan <johanbadillomj@gmail.com>
2025-06-03 12:37:10 -0500

    feat: :sparkles:second commit-add menu

--- Mian.py
+++ Mian.py
@@ -2,3 +2,13 @@
 # ######### Proyecto git prueba #########
 # #####################################
 
+print("Bienvenido a nuestro programa de empanadas Doña Pepa\
+    \n\
+    \n=====================================\
+    \n           Menu Principal             \
+    \n======================================\
+    \nGestion de Datos:\
+    \n1. Agregar dato nuevo\
+    \n2. Editar dato\
+    \n3. Eliminar dato\
+    \n4. Salir")
